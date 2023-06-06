f29acfd91cdbd83934375195651dc78c194f00be
Alessandro Allegranzi <allegranzia@gmail.com>
2023-06-06 07:47:42 -0400

    minor cleanup

--- src/test/UserTest.java
+++ src/test/UserTest.java
@@ -4,11 +4,6 @@ import main.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class UserTest {
 
     @Test
